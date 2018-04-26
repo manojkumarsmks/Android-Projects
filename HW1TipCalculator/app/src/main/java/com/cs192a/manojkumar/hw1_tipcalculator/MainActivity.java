c4034c2968980e0bcf9018f82ba25f691f0c1850
Manoj Kumar Shivaswami <manojkumarsmks@gmail.com>
2018-04-26 16:40:58 -0600

    Calculate the total -> price + tip

--- HW1TipCalculator/app/src/main/java/com/cs192a/manojkumar/hw1_tipcalculator/MainActivity.java
+++ HW1TipCalculator/app/src/main/java/com/cs192a/manojkumar/hw1_tipcalculator/MainActivity.java
@@ -1,18 +1,16 @@
+/* Calculate the tip you pay at your hotel*/
 package com.cs192a.manojkumar.hw1_tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import java.text.DecimalFormat;
-
 public class MainActivity extends AppCompatActivity {
 
     private SeekBar seekBar;
-    private TextView seekBarValue, tipValue;
+    private TextView seekBarValue, tipValue, totalAmount;
     private EditText inputTotal;
 
     private static String TAG = "LOG_VALUE_IS";
@@ -35,16 +33,14 @@ public class MainActivity extends AppCompatActivity {
 
                 // calculate tip only if the total is entered
                 if(!inputTotal.getText().toString().matches("")) {
-                    float total = Float.parseFloat(inputTotal.getText().toString());
-                    if (total > 0) {
-                        tipCalculator(total, (float)progress);
+                    float price = Float.parseFloat(inputTotal.getText().toString());
+                    if (price > 0) {
+                        tipCalculator(price, (float)progress);
                     }
                 }
             }
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
@@ -52,15 +48,21 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
     }
 
-    // Calculate the tip and set the tip value 
-    protected  void tipCalculator(float total, float tipPercentage){
-        tipValue.setText("$"+String.format("%.2f", total*(tipPercentage/100)));
-
+    // Calculate the tip and set the tip value
+    protected  void tipCalculator(float price, float tipPercentage) {
+        float tip = price * (tipPercentage / 100);
+        tipValue.setText("$" + String.format("%.2f", tip));
+        totalCalculator(price, tip);
     }
 
+    // Calculate the total - > fine + tip
+    protected void totalCalculator(float price, float tip) {
+        totalAmount.setText("$"+String.format("%.2f", price +tip));
+    }
+
+    // Initialize the Views
     protected void initialize() {
         seekBar = (SeekBar)findViewById(R.id.percentage_seekbar);
         seekBar.setProgress(0);
@@ -70,6 +72,6 @@ public class MainActivity extends AppCompatActivity {
         inputTotal = (EditText)findViewById(R.id.input_text);
         seekBarValue = (TextView)findViewById(R.id.seekbar_value);
         tipValue = (TextView)findViewById(R.id.tip_value);
+        totalAmount = (TextView)findViewById(R.id.total_amount);
     }
-
 }
