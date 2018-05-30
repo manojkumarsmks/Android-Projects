9996bdffb2349c6aef566e5a68d3b90d7cfdc928
manojkumarsmks <manojkumarsmks@gmail.com>
2018-05-29 22:43:09 -0600

    Pull to Refresh

---
+++ PullToRefresh/app/src/main/java/com/learnandroid/manojkumar/pulltorefresh/Movies.java
@@ -0,0 +1,16 @@
+package com.learnandroid.manojkumar.pulltorefresh;
+
+public class Movies {
+    private String name;
+    public Movies(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
