c20567e649698ce158c40e200fb9a01b712ee785
manojkumarsmks <manojkumarsmks@gmail.com>
2018-06-15 16:22:05 -0600

    Basic recycle view

---
+++ SidebarAlphabets/app/src/main/java/com/skill/manojkumar/sidebaralphabets/CountryAdapter.java
@@ -0,0 +1,49 @@
+package com.skill.manojkumar.sidebaralphabets;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.ViewHolder> {
+
+    private ArrayList<String> countryList;
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView country;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            country = (TextView)itemView.findViewById(R.id.country_name);
+        }
+    }
+
+
+    public CountryAdapter(ArrayList countryList) {
+        this.countryList = countryList;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.country_list_row, parent, false);
+       ViewHolder viewHolder = new ViewHolder(view);
+       return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        if(position == 0)
+
+        holder.country.setText(countryList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return countryList.size();
+    }
+}
---
+++ SidebarAlphabets/app/src/main/java/com/skill/manojkumar/sidebaralphabets/MainActivity.java
@@ -0,0 +1,88 @@
+package com.skill.manojkumar.sidebaralphabets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_STATEMENT = "Lod_Debug";
+    RecyclerView recyclerView;
+    CountryAdapter countryAdapter;
+    RecyclerView.LayoutManager mLayoutManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<String> countryList = new ArrayList<>();
+        recyclerView = (RecyclerView)findViewById(R.id.country_id);
+
+        //Preparing the data
+        countryList.add("Afghanistan");
+        countryList.add("Albania");
+        countryList.add("Angola");
+        countryList.add("Argentina");
+        countryList.add("Armenia");
+        countryList.add("Australia");
+        countryList.add("Bahamas");
+        countryList.add("Bangladesh");
+        countryList.add("Belgium");
+        countryList.add("Combodia");
+        countryList.add("Cameroon");
+        countryList.add("Canada");
+        countryList.add("Cape Verde");
+        countryList.add("Denmark");
+        countryList.add("Dominica");
+        countryList.add("Egypt");
+        countryList.add("Eritrea");
+        countryList.add("Ethiopia");
+        countryList.add("Fiji");
+        countryList.add("France");
+        countryList.add("Finland");
+        countryList.add("Gambia");
+        countryList.add("Georgia");
+        countryList.add("Great Britain");
+        countryList.add("Greece");
+        countryList.add("Haiti");
+        countryList.add("Hungary");
+        countryList.add("Iceland");
+        countryList.add("Iran");
+        countryList.add("Iraq");
+        countryList.add("Italy");
+        countryList.add("Jamaica");
+        countryList.add("Japan");
+        countryList.add("Jordan");
+        countryList.add("Kazakhstan");
+        countryList.add("Kenya");
+        countryList.add("Kosovo");
+        countryList.add("Kuwait");
+        countryList.add("Latvia");
+        countryList.add("Lebanon");
+        countryList.add("Liberia");
+        countryList.add("Libya");
+
+        Collections.sort(countryList, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+
+
+        recyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        countryAdapter = new CountryAdapter(countryList);
+        recyclerView.setAdapter(countryAdapter);
+
+
+    }
+}
